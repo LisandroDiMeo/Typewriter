e40f8f13cffbe15360dffaaabf05ee3f8ad8f320
Lisandro.Diaz <lisandro.diaz@naranja.com>
2022-03-01 01:43:17 -0300

    Reverse animation first approach started

--- src/main/java/com/github/typewriter/Typewriter.kt
+++ src/main/java/com/github/typewriter/Typewriter.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.animation.*
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.animation.doOnEnd
 import com.example.typewriter.R
 import com.github.typewriter.utils.WriterSpeed
 import com.github.typewriter.utils.determineDuration
@@ -14,15 +15,17 @@ constructor(context: Context, attributeSet: AttributeSet? = null, defStyleAttr :
     private var animator: ValueAnimator
     var animationInterpolator : Interpolator = LinearInterpolator()
 
+    private var writerSpeed: WriterSpeed
+    private var textViewContent: String = text.toString()
+
     init {
-        val textViewContent = this.text
         this.text = EMPTY_STRING
         val typedArray = context.obtainStyledAttributes(attributeSet,
             R.styleable.Typewriter, defStyleAttr, 0)
-        val writerSpeed : WriterSpeed = WriterSpeed.values()[typedArray.getInt(R.styleable.Typewriter_writerSpeed, 0)]
+        writerSpeed  = WriterSpeed.values()[typedArray.getInt(R.styleable.Typewriter_writerSpeed, 0)]
         animator = ValueAnimator.ofInt(0, textViewContent.length - 1).apply {
             var index = -1
-            duration = determineDuration(textViewContent.toString(), writerSpeed)
+            duration = determineDuration(textViewContent, writerSpeed)
             addUpdateListener {
                 val currentCharIndex = it.animatedValue as Int
                 if(index != currentCharIndex){
@@ -35,10 +38,31 @@ constructor(context: Context, attributeSet: AttributeSet? = null, defStyleAttr :
         typedArray.recycle()
     }
 
+
+    private fun reverseAnimation(){
+        animator = ValueAnimator.ofInt(textViewContent.length - 1, 0).apply {
+            var index = -1
+            duration = determineDuration(textViewContent, writerSpeed)
+            addUpdateListener {
+                val currentCharIndex = it.animatedValue as Int
+                if(index != currentCharIndex){
+                    val currentChar = textViewContent[currentCharIndex]
+                    this@Typewriter.text = this@Typewriter.text.subSequence(0, currentCharIndex)
+                }
+                index = currentCharIndex
+            }
+        }
+        animator.startDelay = 1000L // Este delay sirve para que el reverso no arranque instantaneamente termina de escribir, si no, nunca llegamos a ver el ultimo caracter.
+        animator.start()
+    }
+
     fun startTypewriter(){
         animator.apply {
             interpolator = animationInterpolator
         }.start()
+        animator.doOnEnd {
+            reverseAnimation()
+        }
     }
 
     companion object {
